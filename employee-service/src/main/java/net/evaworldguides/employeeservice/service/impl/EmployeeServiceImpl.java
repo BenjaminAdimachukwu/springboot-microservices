54f01ca2a0f86bf5907fe73190b1a6e012254fbe
Benjamin Adimachukwu <adimachiben@gmail.com>
2025-06-12 11:54:15 +0100

    added resilience4j Circuitbreaker to the microservices

--- employee-service/src/main/java/net/evaworldguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/evaworldguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package net.evaworldguides.employeeservice.service.impl;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import net.evaworldguides.employeeservice.dto.APIResponseDto;
 import net.evaworldguides.employeeservice.dto.DepartmentDto;
@@ -9,6 +11,8 @@ import net.evaworldguides.employeeservice.repository.EmployeeRepository;
 import net.evaworldguides.employeeservice.service.APIClient;
 import net.evaworldguides.employeeservice.service.EmployeeService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,12 +24,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private   static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private EmployeeRepository employeeRepository;
     //private RestTemplate restTemplate;
     private ModelMapper modelMapper;
-   // private WebClient webClient;
-    private APIClient apiClient;
-
+    private WebClient webClient;
+    //private APIClient apiClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -50,8 +55,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employeeDto1;
     }
 
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+
+        LOGGER.info("inside getEmployeeById() method");
         Employee employee = employeeRepository.findById(employeeId) .orElseThrow(() -> new RuntimeException("Employee not found with id: " + employeeId));;
 
 //        ResponseEntity<DepartmentDto> responseEntity =
@@ -59,11 +68,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 //                        employee.getDepartmentCode(), DepartmentDto.class);
 //        DepartmentDto departmentDto = responseEntity.getBody();
 
-//                 DepartmentDto departmentDto = webClient.get().uri("http://localhost:8080/api/departments/" +
-//                        employee.getDepartmentCode(), DepartmentDto.class)
-//                .retrieve().bodyToMono(DepartmentDto.class).block();
+                 DepartmentDto departmentDto = webClient.get().uri("http://localhost:8080/api/departments/" +
+                        employee.getDepartmentCode(), DepartmentDto.class)
+                .retrieve().bodyToMono(DepartmentDto.class).block();
 
-        DepartmentDto departmentDto =  apiClient.getDepartment(employee.getDepartmentCode());
+        //DepartmentDto departmentDto =  apiClient.getDepartment(employee.getDepartmentCode());
 
 
         EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
@@ -79,5 +88,26 @@ public class EmployeeServiceImpl implements EmployeeService {
         apiResponseDto.setDepartmentDto(departmentDto);
 
         return apiResponseDto;
+    }
+    public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception){
+        LOGGER.info("inside getDefaultDepartment() method");
+        Employee employee = employeeRepository.findById(employeeId) .orElseThrow(() -> new RuntimeException("Employee not found with id: " + employeeId));;
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("R&D Development");
+        departmentDto.setDepartmentCode("RD001");
+        departmentDto.setDepartmentDescription("Research and Development Department");
+        //DepartmentDto departmentDto =  apiClient.getDepartment(employee.getDepartmentCode());
+
+
+        EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployeeDto(employeeDto);
+        apiResponseDto.setDepartmentDto(departmentDto);
+        System.out.println("Fallback method triggered for employeeId: " + employeeId);
+
+        return apiResponseDto;
+
     }
 }
